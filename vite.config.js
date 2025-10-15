5154abeb683ff900ead6f500f76e9c89db3591d4
VisualCryptoLab <victor.garcia.font@gmail.com>
2025-10-15 14:26:08 +0200

    First commit to this repository

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Replace 'REPO-NAME' with your actual GitHub repository name
+  base: '/visualcryptolab/', 
 })
